0cd40a652e14bedf58cecfadbfd88f9b58c8c210
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-04-18 09:27:23 -0500

    Update add_guest(), remove_guest(), and guest_status_menu() functions - Check if floor and room exist in hotel - If the length is equal to 0 check the user in - If the length is greater than 0 check the user out

--- hotel_manager.py
+++ hotel_manager.py
@@ -9,8 +9,6 @@ hotel = {
     '333': ['Neo', 'Trinity', 'Morpheus']
   }
 }
-
-# TODO: Find out if the hotel keys have values.
     
 def check_in_check_out() :
     floor_number = input("Choose a floor number: Floor 1: Room 101, Floor 2: Room 237, or Floor 3: Room 333  ")
@@ -25,31 +23,46 @@ def is_checking_in() :
         
 def add_guest() :
     floor, room = check_in_check_out()
-    occupant_names = is_checking_in()
-    hotel[floor][room] = occupant_names.split(", ")
-    print(f"{occupant_names} Have been checked into {room, floor}")
+    # Check if the floor and room the user provided is in the hotel 
+    if floor in hotel and room in hotel[floor] :
+      # If the room is not occupied check the user in
+      if len(hotel[floor][room]) == 0 :
+        occupant_names = is_checking_in()
+        hotel[floor][room] = occupant_names.split(", ")
+        print(f"{occupant_names} have been checked into {room, floor}")
+      else :
+        print(f"Room: {room} is already occupied.")
+    else :
+      print("That room does not exist.")
     
 def remove_guest() :
     floor, room = check_in_check_out()
-    delete_guest = hotel[floor].pop(room)
-    print(delete_guest)
+    # Check if the floor and room the user provided is in the hotel 
+    if floor in hotel and room in hotel[floor] :
+      # If the room is occupied check the user out
+      if len(hotel[floor][room]) > 0 :
+        delete_guest = hotel[floor][room]
+        hotel[floor][room] = []
+        print(f"{delete_guest} has checked out.")
+    else :
+      print("That room does not exist.")
     
 def display_occupants_and_rooms() :
     print(hotel)
 
 def guest_status_menu():
-    guest_input = input("Are you checking in or out? ")
+    while True:
+      guest_input = input("Are you checking in or out? ")
      
-    if guest_input == "in" :
-        add_guest()
-        display_occupants_and_rooms()
-        exit()
-    elif guest_input == "out" :
-        remove_guest()
-        display_occupants_and_rooms()
-    else :
-        print(f"Please choose in or out")
-                
+      if guest_input == "in" :
+          add_guest()
+          display_occupants_and_rooms()
+      elif guest_input == "out" :
+          remove_guest()
+          display_occupants_and_rooms()
+      else :
+          print(f"Please choose in or out")
+                  
 guest_status_menu()
     
  
